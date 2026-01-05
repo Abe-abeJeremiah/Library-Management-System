913f9286ddd3f2e56082f865fcd7eb63d865e62d
zabtugap2 <zabtugap2@gmail.com>
2026-01-05 16:11:26 +0800

    Update layout and sizes in LoginForm designer

--- LibraryManagementSystem/LoginForm.Designer.cs
+++ LibraryManagementSystem/LoginForm.Designer.cs
@@ -49,9 +49,10 @@ namespace LibraryManagementSystem
             // 
             this.btnLogin.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(179, 373);
+            this.btnLogin.Location = new System.Drawing.Point(239, 459);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(129, 41);
+            this.btnLogin.Size = new System.Drawing.Size(172, 50);
             this.btnLogin.TabIndex = 0;
             this.btnLogin.Text = "LOGIN";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -64,9 +65,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(25, 22);
+            this.lblTitle.Location = new System.Drawing.Point(33, 27);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(395, 33);
+            this.lblTitle.Size = new System.Drawing.Size(497, 42);
             this.lblTitle.TabIndex = 2;
             this.lblTitle.Text = "LibraryManagementSystem";
             this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
@@ -78,9 +80,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblWelcome.AutoSize = true;
             this.lblWelcome.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWelcome.Location = new System.Drawing.Point(119, 96);
+            this.lblWelcome.Location = new System.Drawing.Point(159, 118);
+            this.lblWelcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblWelcome.Name = "lblWelcome";
-            this.lblWelcome.Size = new System.Drawing.Size(257, 33);
+            this.lblWelcome.Size = new System.Drawing.Size(330, 43);
             this.lblWelcome.TabIndex = 3;
             this.lblWelcome.Text = "WELCOME BACK";
             // 
@@ -91,9 +94,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl1.AutoSize = true;
             this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl1.Location = new System.Drawing.Point(54, 152);
+            this.lbl1.Location = new System.Drawing.Point(72, 187);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(160, 16);
+            this.lbl1.Size = new System.Drawing.Size(202, 20);
             this.lbl1.TabIndex = 4;
             this.lbl1.Text = "Please Enter Your Details";
             // 
@@ -103,10 +107,12 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtboxEmailCredentials.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.txtboxEmailCredentials.Location = new System.Drawing.Point(57, 217);
+            this.txtboxEmailCredentials.Location = new System.Drawing.Point(76, 267);
+            this.txtboxEmailCredentials.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtboxEmailCredentials.Name = "txtboxEmailCredentials";
-            this.txtboxEmailCredentials.Size = new System.Drawing.Size(335, 20);
+            this.txtboxEmailCredentials.Size = new System.Drawing.Size(445, 22);
             this.txtboxEmailCredentials.TabIndex = 5;
+            this.txtboxEmailCredentials.TextChanged += new System.EventHandler(this.txtboxEmailCredentials_TextChanged);
             // 
             // txtboxPasswordCredentials
             // 
@@ -114,12 +120,13 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtboxPasswordCredentials.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.txtboxPasswordCredentials.Location = new System.Drawing.Point(57, 274);
-            this.txtboxPasswordCredentials.MaxLength = 5;
+            this.txtboxPasswordCredentials.Location = new System.Drawing.Point(76, 338);
+            this.txtboxPasswordCredentials.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtboxPasswordCredentials.MaxLength = 8;
             this.txtboxPasswordCredentials.Name = "txtboxPasswordCredentials";
-            this.txtboxPasswordCredentials.Size = new System.Drawing.Size(335, 20);
+            this.txtboxPasswordCredentials.Size = new System.Drawing.Size(445, 22);
             this.txtboxPasswordCredentials.TabIndex = 6;
-            this.txtboxPasswordCredentials.Visible = false;
+            this.txtboxPasswordCredentials.UseSystemPasswordChar = true;
             this.txtboxPasswordCredentials.TextChanged += new System.EventHandler(this.txtboxPasswordCredentials_TextChanged);
             // 
             // lbl2
@@ -129,9 +136,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl2.AutoSize = true;
             this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl2.Location = new System.Drawing.Point(53, 194);
+            this.lbl2.Location = new System.Drawing.Point(71, 239);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl2.Name = "lbl2";
-            this.lbl2.Size = new System.Drawing.Size(124, 20);
+            this.lbl2.Size = new System.Drawing.Size(151, 25);
             this.lbl2.TabIndex = 7;
             this.lbl2.Text = "Email Address";
             // 
@@ -142,9 +150,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(53, 251);
+            this.label2.Location = new System.Drawing.Point(71, 309);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.Size = new System.Drawing.Size(106, 25);
             this.label2.TabIndex = 8;
             this.label2.Text = "Password";
             // 
@@ -154,9 +163,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.chckRememberMe.AutoSize = true;
-            this.chckRememberMe.Location = new System.Drawing.Point(67, 329);
+            this.chckRememberMe.Location = new System.Drawing.Point(89, 405);
+            this.chckRememberMe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chckRememberMe.Name = "chckRememberMe";
-            this.chckRememberMe.Size = new System.Drawing.Size(95, 17);
+            this.chckRememberMe.Size = new System.Drawing.Size(119, 20);
             this.chckRememberMe.TabIndex = 10;
             this.chckRememberMe.Text = "Remember Me";
             this.chckRememberMe.UseVisualStyleBackColor = true;
@@ -167,9 +177,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lnkForgotPassword.AutoSize = true;
-            this.lnkForgotPassword.Location = new System.Drawing.Point(300, 333);
+            this.lnkForgotPassword.Location = new System.Drawing.Point(400, 410);
+            this.lnkForgotPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkForgotPassword.Name = "lnkForgotPassword";
-            this.lnkForgotPassword.Size = new System.Drawing.Size(92, 13);
+            this.lnkForgotPassword.Size = new System.Drawing.Size(116, 16);
             this.lnkForgotPassword.TabIndex = 11;
             this.lnkForgotPassword.TabStop = true;
             this.lnkForgotPassword.Text = "Forgot Password?";
@@ -180,9 +191,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(145, 447);
+            this.label1.Location = new System.Drawing.Point(193, 550);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.Size = new System.Drawing.Size(143, 16);
             this.label1.TabIndex = 12;
             this.label1.Text = "Dont have an account?";
             // 
@@ -192,9 +204,10 @@ namespace LibraryManagementSystem
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(285, 447);
+            this.linkLabel1.Location = new System.Drawing.Point(380, 550);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(45, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(55, 16);
             this.linkLabel1.TabIndex = 13;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Sign Up";
@@ -204,9 +217,10 @@ namespace LibraryManagementSystem
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(448, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(597, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(432, 523);
+            this.pictureBox1.Size = new System.Drawing.Size(576, 644);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -215,9 +229,9 @@ namespace LibraryManagementSystem
             // 
             // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 523);
+            this.ClientSize = new System.Drawing.Size(1173, 644);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lnkForgotPassword);
@@ -232,6 +246,7 @@ namespace LibraryManagementSystem
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnLogin);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmLogin";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
