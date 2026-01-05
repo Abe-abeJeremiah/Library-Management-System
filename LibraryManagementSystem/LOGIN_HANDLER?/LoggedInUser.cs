6d175ca6b2ea4d66a497cbae4d73e6b41aa3b9bc
zabtugap2 <zabtugap2@gmail.com>
2026-01-05 16:22:20 +0800

    Add LoggedInUser class for user session management

---
+++ LibraryManagementSystem/LOGIN_HANDLER?/LoggedInUser.cs
@@ -0,0 +1,17 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public static class LoggedInUser
+    {
+        public static int UserID { get; set; }
+        public static string Username { get; set; }
+        public static string Role { get; set; }
+    }
+}
+
