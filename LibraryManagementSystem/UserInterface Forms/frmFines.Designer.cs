7b9b23ea51dbea97f4cdecc867a85c66832615e6
zabtugap2 <zabtugap2@gmail.com>
2026-01-05 16:15:11 +0800

    Refactor frmFines designer code for clarity

--- LibraryManagementSystem/UserInterface Forms/frmFines.Designer.cs
+++ LibraryManagementSystem/UserInterface Forms/frmFines.Designer.cs
@@ -2,22 +2,8 @@ namespace LibraryManagementSystem.UserInterface_Forms
 {
     partial class frmFines
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.Label lblBorrowerId;
-        private System.Windows.Forms.TextBox txtBookID;
-        private System.Windows.Forms.Label lblFine;
-        private System.Windows.Forms.TextBox txtFine;
-        private System.Windows.Forms.Button btnPay;
-        private System.Windows.Forms.Button btnClear;
 
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -29,138 +15,135 @@ namespace LibraryManagementSystem.UserInterface_Forms
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblFine = new System.Windows.Forms.Label();
             this.lblBorrowerId = new System.Windows.Forms.Label();
             this.txtBookID = new System.Windows.Forms.TextBox();
-            this.lblFine = new System.Windows.Forms.Label();
             this.txtFine = new System.Windows.Forms.TextBox();
+            this.txtPaymentDetails = new System.Windows.Forms.TextBox();
+            this.cmbxPaymentMethod = new System.Windows.Forms.ComboBox();
             this.btnPay = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cmbxPaymentMethod = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtPaymentDetails = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lblBorrowerId
+            // label1
             // 
-            this.lblBorrowerId.ForeColor = System.Drawing.Color.White;
-            this.lblBorrowerId.Location = new System.Drawing.Point(283, 69);
-            this.lblBorrowerId.Name = "lblBorrowerId";
-            this.lblBorrowerId.Size = new System.Drawing.Size(100, 23);
-            this.lblBorrowerId.TabIndex = 2;
-            this.lblBorrowerId.Text = "Book ID";
-            this.lblBorrowerId.Click += new System.EventHandler(this.lblBorrowerId_Click);
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(120, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book ID";
             // 
-            // txtBookID
+            // label2
             // 
-            this.txtBookID.Location = new System.Drawing.Point(283, 95);
-            this.txtBookID.Name = "txtBookID";
-            this.txtBookID.Size = new System.Drawing.Size(250, 20);
-            this.txtBookID.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(120, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Payment Method";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(120, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Payment Details";
             // 
             // lblFine
             // 
+            this.lblFine.AutoSize = true;
             this.lblFine.ForeColor = System.Drawing.Color.White;
-            this.lblFine.Location = new System.Drawing.Point(283, 128);
+            this.lblFine.Location = new System.Drawing.Point(120, 190);
             this.lblFine.Name = "lblFine";
-            this.lblFine.Size = new System.Drawing.Size(100, 23);
-            this.lblFine.TabIndex = 8;
-            this.lblFine.Text = "Fine Amount";
+            this.lblFine.Size = new System.Drawing.Size(33, 16);
+            this.lblFine.TabIndex = 3;
+            this.lblFine.Text = "Fine";
+            // 
+            // lblBorrowerId
+            // 
+            this.lblBorrowerId.AutoSize = true;
+            this.lblBorrowerId.Location = new System.Drawing.Point(0, 0);
+            this.lblBorrowerId.Name = "lblBorrowerId";
+            this.lblBorrowerId.Size = new System.Drawing.Size(0, 16);
+            this.lblBorrowerId.TabIndex = 4;
+            // 
+            // txtBookID
+            // 
+            this.txtBookID.Location = new System.Drawing.Point(280, 117);
+            this.txtBookID.Name = "txtBookID";
+            this.txtBookID.ReadOnly = true;
+            this.txtBookID.Size = new System.Drawing.Size(250, 22);
+            this.txtBookID.TabIndex = 5;
             // 
             // txtFine
             // 
-            this.txtFine.Location = new System.Drawing.Point(283, 154);
+            this.txtFine.Location = new System.Drawing.Point(280, 187);
             this.txtFine.Name = "txtFine";
             this.txtFine.ReadOnly = true;
-            this.txtFine.Size = new System.Drawing.Size(250, 20);
-            this.txtFine.TabIndex = 9;
+            this.txtFine.Size = new System.Drawing.Size(250, 22);
+            this.txtFine.TabIndex = 6;
+            // 
+            // txtPaymentDetails
+            // 
+            this.txtPaymentDetails.Location = new System.Drawing.Point(280, 337);
+            this.txtPaymentDetails.Multiline = true;
+            this.txtPaymentDetails.Name = "txtPaymentDetails";
+            this.txtPaymentDetails.ReadOnly = true;
+            this.txtPaymentDetails.Size = new System.Drawing.Size(250, 80);
+            this.txtPaymentDetails.TabIndex = 7;
+            // 
+            // cmbxPaymentMethod
+            // 
+            this.cmbxPaymentMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbxPaymentMethod.FormattingEnabled = true;
+            this.cmbxPaymentMethod.Items.AddRange(new object[] {
+            "Cash",
+            "Gcash",
+            "Paypal"});
+            this.cmbxPaymentMethod.Location = new System.Drawing.Point(280, 257);
+            this.cmbxPaymentMethod.Name = "cmbxPaymentMethod";
+            this.cmbxPaymentMethod.Size = new System.Drawing.Size(250, 24);
+            this.cmbxPaymentMethod.TabIndex = 8;
             // 
             // btnPay
             // 
-            this.btnPay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(167)))), ((int)(((byte)(69)))));
-            this.btnPay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPay.ForeColor = System.Drawing.Color.White;
-            this.btnPay.Location = new System.Drawing.Point(286, 317);
+            this.btnPay.ForeColor = System.Drawing.Color.Black;
+            this.btnPay.Location = new System.Drawing.Point(280, 450);
             this.btnPay.Name = "btnPay";
-            this.btnPay.Size = new System.Drawing.Size(250, 23);
-            this.btnPay.TabIndex = 11;
-            this.btnPay.Text = "Pay Fine";
-            this.btnPay.UseVisualStyleBackColor = false;
+            this.btnPay.Size = new System.Drawing.Size(100, 35);
+            this.btnPay.TabIndex = 9;
+            this.btnPay.Text = "Pay";
+            this.btnPay.UseVisualStyleBackColor = true;
             this.btnPay.Click += new System.EventHandler(this.btnPay_Click);
             // 
             // btnClear
             // 
-            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(53)))), ((int)(((byte)(69)))));
-            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClear.ForeColor = System.Drawing.Color.White;
-            this.btnClear.Location = new System.Drawing.Point(286, 367);
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Location = new System.Drawing.Point(430, 450);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(250, 23);
-            this.btnClear.TabIndex = 12;
+            this.btnClear.Size = new System.Drawing.Size(100, 35);
+            this.btnClear.TabIndex = 10;
             this.btnClear.Text = "Clear";
-            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
-            // 
-            // label1
-            // 
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(283, 189);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Payment Method";
-            // 
-            // cmbxPaymentMethod
-            // 
-            this.cmbxPaymentMethod.FormattingEnabled = true;
-            this.cmbxPaymentMethod.Items.AddRange(new object[] {
-            "Gcash",
-            "Paypal",
-            "Cash"});
-            this.cmbxPaymentMethod.Location = new System.Drawing.Point(286, 215);
-            this.cmbxPaymentMethod.Name = "cmbxPaymentMethod";
-            this.cmbxPaymentMethod.Size = new System.Drawing.Size(247, 21);
-            this.cmbxPaymentMethod.TabIndex = 37;
-            // 
-            // label2
-            // 
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(283, 248);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 23);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Payment Details";
-            // 
-            // txtPaymentDetails
-            // 
-            this.txtPaymentDetails.Location = new System.Drawing.Point(283, 274);
-            this.txtPaymentDetails.Name = "txtPaymentDetails";
-            this.txtPaymentDetails.ReadOnly = true;
-            this.txtPaymentDetails.Size = new System.Drawing.Size(250, 20);
-            this.txtPaymentDetails.TabIndex = 9;
-            // 
-            // label3
-            // 
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(590, 157);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 123);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "irrelevant ang borrower id, kay naka login naman, dapat auto kabalo na";
             // 
             // frmFines
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(1620, 613);
+            this.ClientSize = new System.Drawing.Size(1942, 754);
             this.Controls.Add(this.cmbxPaymentMethod);
             this.Controls.Add(this.lblBorrowerId);
             this.Controls.Add(this.txtBookID);
@@ -173,16 +156,29 @@ namespace LibraryManagementSystem.UserInterface_Forms
             this.Controls.Add(this.btnPay);
             this.Controls.Add(this.btnClear);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmFines";
+            this.Load += new System.EventHandler(this.frmFines_Load_1);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
+
         #endregion
+
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cmbxPaymentMethod;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtPaymentDetails;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblFine;
+        private System.Windows.Forms.Label lblBorrowerId;
+
+        private System.Windows.Forms.TextBox txtBookID;
+        private System.Windows.Forms.TextBox txtFine;
+        private System.Windows.Forms.TextBox txtPaymentDetails;
+
+        private System.Windows.Forms.ComboBox cmbxPaymentMethod;
+
+        private System.Windows.Forms.Button btnPay;
+        private System.Windows.Forms.Button btnClear;
     }
 }
