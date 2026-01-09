bedcdabfe7908d96577fdfc445b56cab293c6dd6
zabtugap2 <zabtugap2@gmail.com>
2026-01-09 17:20:10 +0800

    Update frmBookControler.cs

--- LibraryManagementSystem/UserInterface Forms/frmBookControler.cs
+++ LibraryManagementSystem/UserInterface Forms/frmBookControler.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using LibraryManagementSystem.Model;
 
@@ -15,50 +8,89 @@ namespace LibraryManagementSystem.UserInterface_Forms
 {
     public partial class frmBookControler : Form
     {
+        private Book _currentBook;
+
+        public int BookID => _currentBook?.BookID ?? 0;
+
         public frmBookControler()
         {
             InitializeComponent();
         }
 
-        private void pctBook_Click(object sender, EventArgs e)
-        {
-
-        }
         public void SetData(Book book)
         {
             if (book == null) return;
 
+            _currentBook = book;
+
             lblTitle.Text = book.Title;
-            lblSubTitle.Text = book.SubTitle;
-            lblAuthor.Text = book.Author;
-            lblPages.Text = book.Pages.ToString();
-            lblISBN.Text = book.ISBN;
-            lblLocation.Text = book.Location;
-            lblEditor.Text = book.Editor;
-            lblAccessionNumber.Text = book.AccessionNumber;
-            lblPhysicalDescripton.Text = book.PhysicalDescription;
+            lblSubTitle.Text = string.IsNullOrEmpty(book.SubTitle) ? "" : book.SubTitle;
+            lblAuthor.Text = $"Author: {book.Author}";
+            lblPages.Text = $"Pages: {book.Pages}";
+            lblISBN.Text = $"ISBN: {book.ISBN}";
+            lblLocation.Text = $"Location: {book.Location}";
+            lblEditor.Text = $"Editor: {book.Editor}";
+            lblAccessionNumber.Text = $"Accession #: {book.AccessionNumber}";
+            lblPhysicalDescripton.Text = book.PhysicalDescription ?? "";
             lblPublisher.Text = book.Publisher;
             lblPublicationYear.Text = book.PublicationYear.ToString();
             lblEdition.Text = book.Edition;
             lblLanguage.Text = book.Language;
+            lblCategory.Text = book.Category ?? "";
 
-            // Book image (optional)
-            if (book.CoverImage != null && book.CoverImage.Length > 0)
+            LoadBookImage(book.BookImagePath);
+        }
+
+        private void LoadBookImage(string imagePath)
+        {
+            try
             {
-                using (MemoryStream ms = new MemoryStream(book.CoverImage))
+                pctBook.Image?.Dispose();
+                pctBook.Image = null;
+
+                if (!string.IsNullOrEmpty(imagePath) && File.Exists(imagePath))
                 {
-                    pctBook.Image = Image.FromStream(ms);
+                    using (var fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+                    {
+                        pctBook.Image = Image.FromStream(fs);
+                    }
+
+                }
+                else
+                {
+                     pctBook.Image = Properties.Resources.book; // default image
                 }
             }
-            else
+            catch
             {
-                pctBook.Image = null; // or default image
+                  pctBook.Image = Properties.Resources.book;
             }
         }
 
+
+        private void btnBorrow_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Borrow Book ID: {BookID}", "Borrow");
+            // call Borrow logic here
+        }
+
+        private void btnReserve_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Reserve Book ID: {BookID}", "Reserve");
+            // call Reserve logic here
+        }
+
+        private void pctBook_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(lblTitle.Text, "Book Details");
+        }
         private void pnlBookDetails_Paint(object sender, PaintEventArgs e)
         {
 
         }
+
+        
+
+        
     }
 }
